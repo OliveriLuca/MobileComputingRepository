9c3769ac6fb34d8b4edc4558e6aa80e5d1247f67
MatteoTomassetti25 <147411350+MatteoTomassetti25@users.noreply.github.com>
2023-10-19 12:15:39 +0200

    script quit_game

---
+++ Assets/Scripts/scripts_scelta/quit_game.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit_game : MonoBehaviour
+{
+    public void Quit_game()
+    {
+        Application.Quit();
+    }
+}
