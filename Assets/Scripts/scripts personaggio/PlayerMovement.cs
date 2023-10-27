f6d5e4e139d50480d2c60e4d8576f24a89575808
MatteoTomassetti25 <147411350+MatteoTomassetti25@users.noreply.github.com>
2023-10-27 20:13:14 +0200

    update animazioni

--- Assets/Scripts/scripts personaggio/PlayerMovement.cs
+++ Assets/Scripts/scripts personaggio/PlayerMovement.cs
@@ -9,9 +9,11 @@ public class PlayerMovement : MonoBehaviour
 
     float direction = 0;
 
-    public float speed = 400; 
+    public float speed = 400;
+    bool IsFacingRight = true;
 
-    public Rigidbody2D playerRB; 
+    public Rigidbody2D playerRB;
+    public Animator animator;
 
     private void Awake() 
     {
@@ -34,5 +36,17 @@ public class PlayerMovement : MonoBehaviour
 
         playerRB.velocity = new Vector2(direction * speed * Time.deltaTime, playerRB.velocity.y);
 
+        animator.SetFloat("speed", Mathf.Abs(direction));
+
+        if(IsFacingRight && direction < 0 || !IsFacingRight && direction > 0)
+        {
+            Flip();
+        }
+    }
+
+    void Flip()
+    {
+        IsFacingRight = !IsFacingRight;
+        transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
     }
 }
