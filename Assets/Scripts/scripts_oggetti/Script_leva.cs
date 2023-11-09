4b8bbfdc70c527046b54b7f287eecf2375fe34e1
MatteoTomassetti25 <147411350+MatteoTomassetti25@users.noreply.github.com>
2023-11-09 16:32:03 +0100

    leve livello 3

--- Assets/Scripts/scripts_oggetti/Script_leva.cs
+++ Assets/Scripts/scripts_oggetti/Script_leva.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class Script_leva : MonoBehaviour
 {
     [SerializeField] GameObject personaggio;
-    [SerializeField] GameObject leva;
-    [SerializeField] GameObject leva1;
+    [SerializeField] GameObject animazione_base;
+    [SerializeField] GameObject animazione_attiva;
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject == personaggio)
         {
-            leva.SetActive(false);
-            leva1.SetActive(true);
+            animazione_base.SetActive(false);
+            animazione_attiva.SetActive(true);
         }
     }
 }
