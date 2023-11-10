26685167a552d4f0a382d6a578acdd806897b331
MatteoTomassetti25 <147411350+MatteoTomassetti25@users.noreply.github.com>
2023-11-10 17:57:39 +0100

    livello 6 completato

--- Assets/Scripts/scripts_scelta/here_place.cs
+++ Assets/Scripts/scripts_scelta/here_place.cs
@@ -5,45 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class here_place : MonoBehaviour
 {
-    [SerializeField] GameObject oggetto;
-    [SerializeField] float tempo;
-    [SerializeField] float tempo_fermo;
-    private bool livelloCompletato = false;
-    private float correct_point = 0;
+    [SerializeField] GameObject muro1;
+    [SerializeField] GameObject muro2;
+    
 
 
 
-    private void Update()
-    {
-       while( (oggetto.transform.position.x >= 3 && oggetto.transform.position.x <= 6) && (oggetto.transform.position.y >= 2 && oggetto.transform.position.y <= 2.5))
-        {
-            correct_point++;
-        }
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == oggetto)
+        if (collision.gameObject.name == "Personaggio")
         {
-            Debug.Log("correct_point");
+            muro1.SetActive(true);
+            muro2.SetActive(true);
 
-            if (correct_point == tempo_fermo )
-            {
-                livelloCompletato = true;
-                Invoke("livello_successivo", tempo);
-                
-
-            }
         }
-        
+
 
     }
-
-    private void livello_successivo()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
 
     
 
